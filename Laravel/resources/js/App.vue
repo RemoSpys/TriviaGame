cba256796ab8930a80a174949e99e9459c018205
MikkMaltis <maltismikk@gmail.com>
2024-10-03 15:30:32 +0300

    Commit

---
+++ Laravel/resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <h1>Hello, Vue 3 with Vite!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
